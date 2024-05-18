30c2b861022cb98c4a77aff314b1288ce0db0ed3
Ange <ange.ingabire07@gmail.com>
2024-05-18 23:02:41 +0200

    changes added

--- ride_share_tracking/src/App.js
+++ ride_share_tracking/src/App.js
@@ -1,12 +1,18 @@
-import { LoadScript } from '@react-google-maps/api';
+import { LoadScript, GoogleMap } from '@react-google-maps/api';
 
 function App() {
-  const GOOGLE_MAPS_API_KEY = "AIzaSyDrZExy1e1Lq6D_6Qb1T088LBG4AAPoqlg"; // Replace "YOUR_API_KEY" with your actual API key
+  const GOOGLE_MAPS_API_KEY = "AIzaSyDAN5GIGow9iIg5uwHIWze8Xak_pi8JFJg"; // Replace "YOUR_API_KEY" with your actual API key
 
   return (
     <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
       <div className="App">
-        {/* Your application content goes here */}
+        <h1>Google Maps API Test</h1>
+        <div style={{ width: '100%', height: '400px' }}>
+          <GoogleMap
+            center={{ lat: (-1.9355377074007851, 30.060163829002217) }}
+            zoom={12}
+          />
+        </div>
       </div>
     </LoadScript>
   );
